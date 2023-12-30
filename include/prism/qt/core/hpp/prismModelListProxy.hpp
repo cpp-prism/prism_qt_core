9f6218ca5de5ca32f01f1335d4a7688e7af5ba63
hbb_mac <kok.bing@qq.com>
2023-12-30 16:36:55 +0800

    code style - .

--- include/prism/qt/core/hpp/prismModelListProxy.hpp
+++ include/prism/qt/core/hpp/prismModelListProxy.hpp
@@ -8,38 +8,66 @@
 #include <prism/prism.hpp>
 #include <type_traits>
 
-namespace prism::qt::core {
-
-class PRISMQT_CORE_EXPORT prismModelListProxyBase:public QAbstractListModel
+namespace prism::qt::core
 {
-   Q_OBJECT
-public:
-    prismModelListProxyBase(QObject* parent =nullptr){ Q_UNUSED(parent) }
-    virtual ~prismModelListProxyBase(){}
-    Q_INVOKABLE virtual  QVariant getRowData(int index) { Q_UNUSED(index) return QVariant(); }
-    Q_INVOKABLE virtual  QVariant getRowData(const QModelIndex& index) { Q_UNUSED(index) return QVariant(); }
-    Q_INVOKABLE virtual int length() { return 0; }
-    Q_INVOKABLE virtual int indexOfValue(QString fname ,QVariant value){ Q_UNUSED(fname)  Q_UNUSED(value) return 0;}
-    Q_INVOKABLE virtual void pub_beginResetModel(){}
-    Q_INVOKABLE virtual void pub_endResetModel(){}
+
+class PRISMQT_CORE_EXPORT prismModelListProxyBase : public QAbstractListModel
+{
+    Q_OBJECT
+  public:
+    prismModelListProxyBase(QObject* parent = nullptr)
+    {
+        Q_UNUSED(parent)
+    }
+    virtual ~prismModelListProxyBase()
+    {
+    }
+    Q_INVOKABLE virtual QVariant getRowData(int index)
+    {
+        Q_UNUSED(index)
+        return QVariant();
+    }
+    Q_INVOKABLE virtual QVariant getRowData(const QModelIndex& index)
+    {
+        Q_UNUSED(index)
+        return QVariant();
+    }
+    Q_INVOKABLE virtual int length()
+    {
+        return 0;
+    }
+    Q_INVOKABLE virtual int indexOfValue(QString fname, QVariant value)
+    {
+        Q_UNUSED(fname)
+        Q_UNUSED(value)
+        return 0;
+    }
+    Q_INVOKABLE virtual void pub_beginResetModel()
+    {
+    }
+    Q_INVOKABLE virtual void pub_endResetModel()
+    {
+    }
 };
 
 template <typename T>
-class prismModelListProxy:public prismModelListProxyBase
+class prismModelListProxy : public prismModelListProxyBase
 {
 
     QVector<prismModelProxy<T>*>* m_list = nullptr;
-public:
+
+  public:
     using value_type = T;
-    explicit prismModelListProxy(QObject* parent = nullptr) :prismModelListProxyBase(parent),  m_list(new QVector<prismModelProxy<T>*>())
+    explicit prismModelListProxy(QObject* parent = nullptr)
+        : prismModelListProxyBase(parent), m_list(new QVector<prismModelProxy<T>*>())
     {
         Q_UNUSED(parent)
-        QQmlEngine::setObjectOwnership(this,QQmlEngine::ObjectOwnership::CppOwnership);
+        QQmlEngine::setObjectOwnership(this, QQmlEngine::ObjectOwnership::CppOwnership);
     }
 
     ~prismModelListProxy()
     {
-        if(m_list)
+        if (m_list)
         {
             delete m_list;
             m_list = nullptr;
@@ -48,34 +76,37 @@ public:
 
     QVariant getRowData(const QModelIndex& index) override
     {
-        if(!index.isValid())
+        if (!index.isValid())
             return QVariant();
         int idx = index.row();
-        if(idx<0)
+        if (idx < 0)
             return QVariant();
-        if(idx >= m_list->size())
+        if (idx >= m_list->size())
             return QVariant();
-        if(m_list)
-            return QVariant::fromValue( m_list->at(idx));
+        if (m_list)
+            return QVariant::fromValue(m_list->at(idx));
 
         return QVariant();
     }
     QVariant getRowData(int index) override
     {
-        if(index<0)
+        if (index < 0)
             return QVariant();
-        if(index >= m_list->size())
+        if (index >= m_list->size())
             return QVariant();
-        if(m_list)
-            return QVariant::fromValue( m_list->at(index));
+        if (m_list)
+            return QVariant::fromValue(m_list->at(index));
 
         return QVariant();
     }
 
-    QVector<prismModelProxy<T>*>* list() const { return m_list; }
+    QVector<prismModelProxy<T>*>* list() const
+    {
+        return m_list;
+    }
 
     // Basic functionality:
-    int rowCount(const QModelIndex& parent = QModelIndex()) const
+    int rowCount(const QModelIndex& parent = QModelIndex()) const override
     {
         if (parent.isValid() || !list())
             return 0;
@@ -83,15 +114,15 @@ public:
         return list()->size();
     }
 
-    virtual QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const
+    virtual QVariant data(const QModelIndex& index, int role = Qt::DisplayRole) const override
     {
         Q_UNUSED(role)
         if (!index.isValid() || !list())
             return QVariant();
         prismModelProxy<T>* tm = m_list->at(index.row());
 
-        //todo
-        return  tm->get(roleNames()[role].data());
+        // todo
+        return tm->get(roleNames()[role].data());
     }
 
     // Editable:
@@ -101,7 +132,7 @@ public:
             return false;
         prismModelProxy<T>* tm = m_list->at(index.row());
 
-        tm->set(roleNames()[role].data(),value.value<T>());
+        tm->set(roleNames()[role].data(), value.value<T>());
         emit dataChanged(index, index, {role});
         return true;
     }
@@ -116,12 +147,12 @@ public:
         prismModelProxy<T>* tm = m_list->at(index);
 
         int role = nameRoles()[roleName];
-        tm->set(roleName.data(),value);
+        tm->set(roleName.data(), value);
         emit dataChanged(this->index(index, 0), this->index(index, 0), {role});
         return true;
     }
 
-    Qt::ItemFlags flags(const QModelIndex& index) const
+    Qt::ItemFlags flags(const QModelIndex& index) const override
     {
         if (!index.isValid())
             return Qt::NoItemFlags;
@@ -129,34 +160,31 @@ public:
         return Qt::ItemIsEditable; // FIXME: Implement me!
     }
 
-
     QHash<int, QByteArray> roleNames() const override
     {
         static QHash<int, QByteArray> roles;
         if (roles.count() == 0)
         {
-            QHash<int,QByteArray> roles2;
+            QHash<int, QByteArray> roles2;
             int role1 = 500;
-            prism::reflection::for_each_fields(*static_cast<T*>(nullptr),[&](const char* name,auto&v){
+            prism::reflection::for_each_fields(*static_cast<T*>(nullptr), [&](const char* name, auto& v) {
                 Q_UNUSED(v)
                 roles2[role1++] = name;
-
             });
             roles = roles2;
         }
         return roles;
     }
-     QHash<QByteArray, int> nameRoles() const
+    QHash<QByteArray, int> nameRoles() const
     {
         static QHash<QByteArray, int> names;
         static int role2;
         if (names.count() == 0)
         {
             role2 = 500;
-            prism::reflection::for_each_fields(*static_cast<T*>(nullptr),[&](const char* name,auto& v)
-            {
+            prism::reflection::for_each_fields(*static_cast<T*>(nullptr), [&](const char* name, auto& v) {
                 Q_UNUSED(v)
-                names[name] = role2++ ;
+                names[name] = role2++;
             });
         }
         return names;
@@ -164,12 +192,12 @@ public:
     virtual void appendItem(std::shared_ptr<T> tm)
     {
         beginInsertRows(QModelIndex(), m_list->size(), m_list->size());
-        m_list->append(new prismModelProxy<T>(this,tm));
+        m_list->append(new prismModelProxy<T>(this, tm));
         endInsertRows();
     }
     virtual void appendItemNotNotify(std::shared_ptr<T> tm)
     {
-        m_list->append(new prismModelProxy<T>(this,tm));
+        m_list->append(new prismModelProxy<T>(this, tm));
     }
     virtual void removeAllItemsNotNotify()
     {
@@ -196,17 +224,18 @@ public:
         endRemoveRows();
     }
 
-    int length() override{
-        return m_list?m_list->size():0;
-    }
-    int indexOfValue(QString fname,QVariant value) override
+    int length() override
     {
-        if(value.isNull()|| !value.isValid()|| list()->size() ==0 )
+        return m_list ? m_list->size() : 0;
+    }
+    int indexOfValue(QString fname, QVariant value) override
+    {
+        if (value.isNull() || !value.isValid() || list()->size() == 0)
             return -1;
-        for(int i=0; i < list()->size();++i)
+        for (int i = 0; i < list()->size(); ++i)
         {
             prismModelProxy<T>* item = list()->at(i);
-            if(value ==item->get(fname.toStdString().data()))
+            if (value == item->get(fname.toStdString().data()))
             {
                 return i;
             }
@@ -215,7 +244,7 @@ public:
     }
 
     // prismModelListProxyBase interface
-public:
+  public:
     void pub_beginResetModel() override
     {
         this->beginResetModel();
@@ -225,5 +254,5 @@ public:
         this->endResetModel();
     }
 };
-}// namespace prism::qt::core
+} // namespace prism::qt::core
 #endif // PRISM_QT_CORE_HPP_PRISMMODELLISTPROXY_HPP
--- include/prism/qt/core/hpp/prismModelProxy.hpp
+++ include/prism/qt/core/hpp/prismModelProxy.hpp
@@ -5,18 +5,22 @@
 #include "prism/prism.hpp"
 #include <QDebug>
 #include <QObject>
+#include <QQmlEngine>
 #include <QVariant>
 #include <memory>
-#include <QQmlEngine>
 
+namespace prism::qt::core
+{
 
-namespace prism::qt::core {
-
-#ifdef __linux
-template<class TV> constexpr inline void get_field_do(QList<const char*>& fns,TV& p_v,QVariant& qv);
-template<class TV> constexpr inline bool set_field_do(QList<const char*>& fns,TV& p_v, QVariant& value);
-template<class TV> constexpr inline bool recurseSetValue(QList<const char*>& fns,TV& p_v, QVariant& value);
-template<class TV> constexpr inline void recurseGetValue(QList<const char*>& fns,TV& p_v,QVariant& qv);
+#if defined(__linux) || defined(__clang__)
+template <class TV>
+constexpr inline void get_field_do(QList<const char*>& fns, TV& p_v, QVariant& qv);
+template <class TV>
+constexpr inline bool set_field_do(QList<const char*>& fns, TV& p_v, QVariant& value);
+template <class TV>
+constexpr inline bool recurseSetValue(QList<const char*>& fns, TV& p_v, QVariant& value);
+template <class TV>
+constexpr inline void recurseGetValue(QList<const char*>& fns, TV& p_v, QVariant& qv);
 #endif
 
 template <class T>
@@ -29,38 +33,49 @@ struct has_def_indexer
     static constexpr bool value = decltype(test<T>(nullptr))::value;
 };
 
-template<class T>
-void getTransValue(const char* fname,T& model,QVariant& qv)
+template <class T>
+void getTransValue([[maybe_unused]] const char* fname, [[maybe_unused]] T& model, [[maybe_unused]] QVariant& qv)
 {
-
 }
 
-class PRISMQT_CORE_EXPORT prismModelProxyBase:public QObject
+class PRISMQT_CORE_EXPORT prismModelProxyBase : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(bool notifyFlag READ notifyFlag WRITE setNotifyFlag NOTIFY notifyFlagChanged)
 
     bool m_notifyFlag = false;
 
-public:
-    virtual ~prismModelProxyBase() {}
-    explicit prismModelProxyBase(QObject* parent =nullptr):QObject(parent) {
-        QQmlEngine::setObjectOwnership(this,QQmlEngine::ObjectOwnership::CppOwnership);
+  public:
+    virtual ~prismModelProxyBase()
+    {
+    }
+    explicit prismModelProxyBase(QObject* parent = nullptr)
+        : QObject(parent)
+    {
+        QQmlEngine::setObjectOwnership(this, QQmlEngine::ObjectOwnership::CppOwnership);
     }
 
-    bool notifyFlag() const {
+    bool notifyFlag() const
+    {
         return m_notifyFlag;
     }
 
-public slots:
+  public slots:
 
-    Q_INVOKABLE virtual   QVariant get(const QByteArray& fname) { Q_UNUSED(fname) return QVariant(); }
-    Q_INVOKABLE virtual  void set(const QByteArray& fname,QVariant value){ Q_UNUSED(fname) Q_UNUSED(value) }
+    Q_INVOKABLE virtual QVariant get(const QByteArray& fname)
+    {
+        Q_UNUSED(fname)
+        return QVariant();
+    }
+    Q_INVOKABLE virtual void set(const QByteArray& fname, QVariant value)
+    {
+        Q_UNUSED(fname)
+        Q_UNUSED(value)
+    }
     Q_INVOKABLE void update()
     {
         setNotifyFlag(!notifyFlag());
     }
-
 
     void setNotifyFlag(bool notifyFlag)
     {
@@ -70,15 +85,14 @@ public slots:
         m_notifyFlag = notifyFlag;
         emit notifyFlagChanged(m_notifyFlag);
     }
-signals:
+  signals:
     void notifyFlagChanged(bool notifyFlag);
 };
 
-
-template<class T>
-class  prismModelProxy:public prismModelProxyBase
+template <class T>
+class prismModelProxy : public prismModelProxyBase
 {
-public:
+  public:
     using value_type = T;
     std::shared_ptr<T> instance()
     {
@@ -89,92 +103,88 @@ public:
         m_instance = newInstance;
     }
     std::shared_ptr<T> m_instance = nullptr;
-    virtual ~prismModelProxy<T>(){}
-    prismModelProxy<T>(QObject* parent = nullptr,std::shared_ptr<T> instance=std::make_shared<T>())
-        :prismModelProxyBase(parent)
-        ,m_instance(instance)
+    virtual ~prismModelProxy<T>()
     {
-        QQmlEngine::setObjectOwnership(this,QQmlEngine::ObjectOwnership::CppOwnership);
     }
-
+    prismModelProxy<T>(QObject* parent = nullptr, std::shared_ptr<T> instance = std::make_shared<T>())
+        : prismModelProxyBase(parent), m_instance(instance)
+    {
+        QQmlEngine::setObjectOwnership(this, QQmlEngine::ObjectOwnership::CppOwnership);
+    }
 
     // prismModelProxyBase interface
     QVariant get(const QByteArray& fname) override
     {
         QVariant qv;
-        if(!m_instance)
+        if (!m_instance)
             return qv;
 
-        QList<QByteArray> fns =fname.simplified().replace(" ","").split('.');
-        QList<const char*> fns2 ;
-        for(QByteArray& item : fns)
+        QList<QByteArray> fns = fname.simplified().replace(" ", "").split('.');
+        QList<const char*> fns2;
+        for (QByteArray& item : fns)
         {
             fns2.append(item);
         }
 
-        recurseGetValue<T>(fns2,*m_instance.get(),qv);
+        recurseGetValue<T>(fns2, *m_instance.get(), qv);
         return qv;
 
-        //for(const QByteArray& fn :fns)
+        // for(const QByteArray& fn :fns)
         //{
-        //    prism::reflection::field_do(*m_instance,fn.data(),[&](auto& v){
-        //        qv = QVariant::fromValue(v);
-        //    });
-        //}
-        //return qv;
-
+        //     prism::reflection::field_do(*m_instance,fn.data(),[&](auto& v){
+        //         qv = QVariant::fromValue(v);
+        //     });
+        // }
+        // return qv;
     }
 
-    void set(const QByteArray &fname, QVariant value) override
+    void set(const QByteArray& fname, QVariant value) override
     {
-        //qDebug()<< "set " << fname;
-        if(!m_instance)
+        // qDebug()<< "set " << fname;
+        if (!m_instance)
             return;
 
-        QList<QByteArray> fns =fname.simplified().replace(" ","").split('.');
-        QList<const char*> fns2 ;
-        for(QByteArray& item : fns)
+        QList<QByteArray> fns = fname.simplified().replace(" ", "").split('.');
+        QList<const char*> fns2;
+        for (QByteArray& item : fns)
         {
             fns2.append(item);
         }
 
-        if(recurseSetValue<T>(fns2,*m_instance.get(),value))
+        if (recurseSetValue<T>(fns2, *m_instance.get(), value))
             update();
 
-        //prism::reflection::field_do(*m_instance,fname.data(),[&](auto& v){
-        //    if(!value.canConvert<std::remove_reference_t< decltype(v)>>())
-        //    {
-        //        qDebug()<< "设置值失败,类型错误";
-        //        return;
-        //    }
-        //    v =  value.value<std::remove_reference_t<decltype(v)>>();
-        //});
-
+        // prism::reflection::field_do(*m_instance,fname.data(),[&](auto& v){
+        //     if(!value.canConvert<std::remove_reference_t< decltype(v)>>())
+        //     {
+        //         qDebug()<< "设置值失败,类型错误";
+        //         return;
+        //     }
+        //     v =  value.value<std::remove_reference_t<decltype(v)>>();
+        // });
     }
 };
 
-
-
-
-template<class TV> constexpr inline void get_field_do(QList<const char*>& fns,TV& p_v,QVariant& qv)
+template <class TV>
+constexpr inline void get_field_do(QList<const char*>& fns, TV& p_v, QVariant& qv)
 {
-    if(fns.length()==0)
+    if (fns.length() == 0)
     {
-        if constexpr (prism::utilities::is_specialization<TV,std::shared_ptr>::value ||
-               prism::utilities::is_specialization<TV,std::unique_ptr>::value )
+        if constexpr (prism::utilities::is_specialization<TV, std::shared_ptr>::value ||
+                      prism::utilities::is_specialization<TV, std::unique_ptr>::value)
         {
             qv = QVariant::fromValue(p_v.get());
         }
-        else if constexpr(prism::utilities::is_specialization<TV,std::weak_ptr>::value )
+        else if constexpr (prism::utilities::is_specialization<TV, std::weak_ptr>::value)
         {
             qv = QVariant::fromValue(p_v.lock().get());
         }
-        else if constexpr(prism::utilities::has_def<prism::enums::enum_info<TV>>::value)
+        else if constexpr (prism::utilities::has_def<prism::enums::enum_info<TV>>::value)
         {
             const char* str = prism::enums::enum_info<TV>::tostring(p_v);
             qv = QVariant::fromValue(QString::fromStdString(str));
         }
-        else if constexpr(std::is_same_v<std::string,TV>)
+        else if constexpr (std::is_same_v<std::string, TV>)
         {
             qv = QVariant::fromValue(QString::fromStdString(p_v));
         }
@@ -183,100 +193,103 @@ template<class TV> constexpr inline void get_field_do(QList<const char*>& fns,TV
             qv = QVariant::fromValue(p_v);
         }
     }
-    else{
-        recurseGetValue(fns,p_v,qv);
+    else
+    {
+        recurseGetValue(fns, p_v, qv);
     }
 }
 
-template<class TV> constexpr inline bool set_field_do(QList<const char*>& fns,TV& p_v, QVariant& value)
+template <class TV>
+constexpr inline bool set_field_do(QList<const char*>& fns, TV& p_v, QVariant& value)
 {
     using model_t = std::remove_reference_t<decltype(p_v)>;
-    if(fns.length()==0)
+    if (fns.length() == 0)
     {
-        if(!value.canConvert<model_t>())
+        if (!value.canConvert<model_t>())
         {
-            qDebug()<< "设置值失败,类型错误,不能把" << value << QString("设置给[%1]类型的变量")
-                       .arg(QString::fromStdString(std::string(utilities::typeName<decltype(p_v)>::name())));
+            qDebug() << "设置值失败,类型错误,不能把" << value << QString("设置给[%1]类型的变量").arg(QString::fromStdString(std::string(utilities::typeName<decltype(p_v)>::name())));
             return false;
         }
         auto v = value.value<model_t>();
-        if(v != p_v)
+        if (v != p_v)
         {
-            p_v =  v;
+            p_v = v;
             return true;
         }
         else
             return false;
     }
-    else{
-        return recurseSetValue(fns,p_v,value);
+    else
+    {
+        return recurseSetValue(fns, p_v, value);
     }
-
 }
-template<class TV> constexpr inline bool recurseSetValue(QList<const char*>& fns,TV& p_v, QVariant& value)
+template <class TV>
+constexpr inline bool recurseSetValue(QList<const char*>& fns, TV& p_v, QVariant& value)
 {
     const char* fname = fns[0];
     fns.removeAt(0);
     bool isUpdate = false;
-    if constexpr (std::is_pointer_v<TV>||
-            prism::utilities::is_specialization<TV,std::shared_ptr>::value ||
-            prism::utilities::is_specialization<TV,std::unique_ptr>::value  )
+    if constexpr (std::is_pointer_v<TV> ||
+                  prism::utilities::is_specialization<TV, std::shared_ptr>::value ||
+                  prism::utilities::is_specialization<TV, std::unique_ptr>::value)
     {
-        prism::reflection::field_do(*p_v,fname,[&](auto& v){
-            isUpdate |= set_field_do(fns,v,value);
+        prism::reflection::field_do(*p_v, fname, [&](auto& v) {
+            isUpdate |= set_field_do(fns, v, value);
         });
     }
-    else if constexpr ( prism::utilities::is_specialization<TV,std::weak_ptr>::value  )
+    else if constexpr (prism::utilities::is_specialization<TV, std::weak_ptr>::value)
     {
-        prism::reflection::field_do(*p_v.lock(),fname,[&](auto& v){
-            isUpdate |= set_field_do(fns,v,value);
+        prism::reflection::field_do(*p_v.lock(), fname, [&](auto& v) {
+            isUpdate |= set_field_do(fns, v, value);
         });
     }
-    else if constexpr ( prism::utilities::is_specialization<TV,std::optional>::value  )
+    else if constexpr (prism::utilities::is_specialization<TV, std::optional>::value)
     {
-        if(p_v.has_value())
+        if (p_v.has_value())
         {
-            prism::reflection::field_do(p_v.value(),fname,[&](auto& v){
-                isUpdate |= set_field_do(fns,v,value);
+            prism::reflection::field_do(p_v.value(), fname, [&](auto& v) {
+                isUpdate |= set_field_do(fns, v, value);
             });
         }
     }
     else
     {
-        prism::reflection::field_do(p_v,fname,[&](auto& v){
-            isUpdate |= set_field_do(fns,v,value);
+        prism::reflection::field_do(p_v, fname, [&](auto& v) {
+            isUpdate |= set_field_do(fns, v, value);
         });
     }
 
     return isUpdate;
 }
-template<class TV> constexpr inline void recurseGetValue(QList<const char*>& fns,TV& p_v,QVariant& qv)
+template <class TV>
+constexpr inline void recurseGetValue(QList<const char*>& fns, TV& p_v, QVariant& qv)
 {
     const char* fname = fns[0];
     fns.removeAt(0);
-    if constexpr (prism::utilities::is_specialization<TV,std::weak_ptr>::value)
+    if constexpr (prism::utilities::is_specialization<TV, std::weak_ptr>::value)
     {
-        using tt = std::remove_reference_t< std::remove_reference_t< decltype(*(p_v.lock())) > >;
-        prism::reflection::field_do<0,tt>(*p_v.lock(),fname,[&](auto& v){
-            get_field_do(fns,v,qv);
+        using tt = std::remove_reference_t<std::remove_reference_t<decltype(*(p_v.lock()))>>;
+        prism::reflection::field_do<0, tt>(*p_v.lock(), fname, [&](auto& v) {
+            get_field_do(fns, v, qv);
         });
     }
-    else if constexpr (std::is_pointer_v<TV>||
-            prism::utilities::is_specialization<TV,std::shared_ptr>::value ||
-            prism::utilities::is_specialization<TV,std::unique_ptr>::value  )
+    else if constexpr (std::is_pointer_v<TV> ||
+                       prism::utilities::is_specialization<TV, std::shared_ptr>::value ||
+                       prism::utilities::is_specialization<TV, std::unique_ptr>::value)
     {
-        prism::reflection::field_do<0,std::remove_reference_t<std::remove_reference_t<decltype(*p_v)>>>(*p_v,fname,[&](auto& v){
-            get_field_do(fns,v,qv);
+        prism::reflection::field_do<0, std::remove_reference_t<std::remove_reference_t<decltype(*p_v)>>>(*p_v, fname, [&](auto& v) {
+            get_field_do(fns, v, qv);
         });
     }
     else
     {
-        prism::reflection::field_do(p_v,fname,[&](auto& v){
-            get_field_do(fns,v,qv);
+        prism::reflection::field_do(p_v, fname, [&](auto& v) {
+            get_field_do(fns, v, qv);
         });
     }
 }
 
-}// namespace prism::qt::core
+} // namespace prism::qt::core
 
 #endif // PRISM_QT_CORE_HPP_PRISMMODELPROXY_H
