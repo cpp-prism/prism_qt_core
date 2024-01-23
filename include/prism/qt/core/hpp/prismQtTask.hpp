e96d7d71189118ab79eb8cd08ae0b791a7778187
huangbinbin <huangbinbin@deepvision-tech.com>
2024-01-23 19:10:32 +0800

    chore - comment prismTask's methoed 'thenAsync'

--- include/prism/qt/core/hpp/prismQtTask.hpp
+++ include/prism/qt/core/hpp/prismQtTask.hpp
@@ -54,58 +54,59 @@ class  prismQtTaskBase : public QObject, public std::enable_shared_from_this<pri
         });
         w.setFuture(futureResult);
     }
-    template <typename F>
-    auto thenAsync(F ptr_run) -> std::enable_if_t<std::is_same_v<decltype(ptr_run()), void>, std::shared_ptr<prismQtTaskBase<void>>>
-    {
-        std::shared_ptr<prismQtTaskBase<void>> t = std::make_shared<prismQtTaskBase<void>>();
-        w.setFuture(futureResult);
-        auto self = this->shared_from_this();
-        t->futureResult = QtConcurrent::run([self, ptr_run, t] {
-            while (!self->futureResult.isFinished())
-                QCoreApplication::processEvents();
-            try
-            {
-                ptr_run();
-            }
-            catch (std::exception& ex)
-            {
-                qDebug() << QString::fromStdString(std::string(ex.what()));
-            }
-            catch (...)
-            {
-                qDebug() << "unknow exception";
-            }
-            self->w.disconnect();
-        });
-        return t;
-    }
 
-    template <typename F>
-    auto thenAsync(F ptr_run) -> std::enable_if_t<!std::is_same_v<decltype(ptr_run()), void>, std::shared_ptr<prismQtTask<decltype(ptr_run())>>>
-    {
-        auto self = this->shared_from_this();
-        std::shared_ptr<prismQtTask<decltype(ptr_run())>> t = std::make_shared<prismQtTask<decltype(ptr_run())>>();
-        w.setFuture(futureResult);
-        t->futureResult = QtConcurrent::run([self, ptr_run] {
-            while (!self->futureResult.isFinished())
-                QCoreApplication::processEvents();
-            try
-            {
-                return ptr_run();
-            }
-            catch (std::exception& ex)
-            {
-                qDebug() << QString::fromStdString(std::string(ex.what()));
-            }
-            catch (...)
-            {
-                qDebug() << "unknow exception";
-            }
-            self->w.disconnect();
-            return decltype(ptr_run())();
-        });
-        return t;
-    }
+    //template <typename F>
+    //auto thenAsync(F ptr_run) -> std::enable_if_t<std::is_same_v<decltype(ptr_run()), void>, std::shared_ptr<prismQtTaskBase<void>>>
+    //{
+    //    std::shared_ptr<prismQtTaskBase<void>> t = std::make_shared<prismQtTaskBase<void>>();
+    //    w.setFuture(futureResult);
+    //    auto self = this->shared_from_this();
+    //    t->futureResult = QtConcurrent::run([self, ptr_run, t] {
+    //        while (!self->futureResult.isFinished())
+    //            QCoreApplication::processEvents();
+    //        try
+    //        {
+    //            ptr_run();
+    //        }
+    //        catch (std::exception& ex)
+    //        {
+    //            qDebug() << QString::fromStdString(std::string(ex.what()));
+    //        }
+    //        catch (...)
+    //        {
+    //            qDebug() << "unknow exception";
+    //        }
+    //        self->w.disconnect();
+    //    });
+    //    return t;
+    //}
+
+    //template <typename F>
+    //auto thenAsync(F ptr_run) -> std::enable_if_t<!std::is_same_v<decltype(ptr_run()), void>, std::shared_ptr<prismQtTask<decltype(ptr_run())>>>
+    //{
+    //    auto self = this->shared_from_this();
+    //    std::shared_ptr<prismQtTask<decltype(ptr_run())>> t = std::make_shared<prismQtTask<decltype(ptr_run())>>();
+    //    w.setFuture(futureResult);
+    //    t->futureResult = QtConcurrent::run([self, ptr_run] {
+    //        while (!self->futureResult.isFinished())
+    //            QCoreApplication::processEvents();
+    //        try
+    //        {
+    //            return ptr_run();
+    //        }
+    //        catch (std::exception& ex)
+    //        {
+    //            qDebug() << QString::fromStdString(std::string(ex.what()));
+    //        }
+    //        catch (...)
+    //        {
+    //            qDebug() << "unknow exception";
+    //        }
+    //        self->w.disconnect();
+    //        return decltype(ptr_run())();
+    //    });
+    //    return t;
+    //}
 };
 
 template <typename T>
@@ -136,59 +137,59 @@ class  prismQtTask : public prismQtTaskBase<T>
         w.setFuture(futureResult);
     }
 
-    template <typename F>
-    auto thenAsync(F ptr_run) -> std::enable_if_t<std::is_same_v<decltype(ptr_run(T())), void>, std::shared_ptr<prismQtTaskBase<void>>>
-    {
-        std::shared_ptr<prismQtTaskBase<void>> t = std::make_shared<prismQtTaskBase<void>>();
-        w.setFuture(futureResult);
-        auto self = this->shared_from_this();
-        t->futureResult = QtConcurrent::run([self, ptr_run, t] {
-            while (!self->futureResult.isFinished())
-                QCoreApplication::processEvents();
-            try
-            {
-                ptr_run(self->w.result());
-            }
-            catch (std::exception& ex)
-            {
-                qDebug() << QString::fromStdString(std::string(ex.what()));
-            }
-            catch (...)
-            {
-                qDebug() << "unknow exception";
-            }
-            self->w.disconnect();
-            return decltype(ptr_run(T()))();
-        });
-        return t;
-    }
+    //template <typename F>
+    //auto thenAsync(F ptr_run) -> std::enable_if_t<std::is_same_v<decltype(ptr_run(T())), void>, std::shared_ptr<prismQtTaskBase<void>>>
+    //{
+    //    std::shared_ptr<prismQtTaskBase<void>> t = std::make_shared<prismQtTaskBase<void>>();
+    //    w.setFuture(futureResult);
+    //    auto self = this->shared_from_this();
+    //    t->futureResult = QtConcurrent::run([self, ptr_run, t] {
+    //        while (!self->futureResult.isFinished())
+    //            QCoreApplication::processEvents();
+    //        try
+    //        {
+    //            ptr_run(self->w.result());
+    //        }
+    //        catch (std::exception& ex)
+    //        {
+    //            qDebug() << QString::fromStdString(std::string(ex.what()));
+    //        }
+    //        catch (...)
+    //        {
+    //            qDebug() << "unknow exception";
+    //        }
+    //        self->w.disconnect();
+    //        return decltype(ptr_run(T()))();
+    //    });
+    //    return t;
+    //}
 
-    template <typename F>
-    auto thenAsync(F ptr_run) -> std::enable_if_t<!std::is_same_v<decltype(ptr_run(T())), void>, std::shared_ptr<prismQtTask<decltype(ptr_run(T()))>>>
-    {
-        auto self = this->shared_from_this();
-        std::shared_ptr<prismQtTask<decltype(ptr_run(T()))>> t = std::make_shared<prismQtTask<decltype(ptr_run(T()))>>();
-        w.setFuture(futureResult);
-        t->futureResult = QtConcurrent::run([self, ptr_run] {
-            while (!self->futureResult.isFinished())
-                QCoreApplication::processEvents();
-            try
-            {
-                return ptr_run(self->w.result());
-            }
-            catch (std::exception& ex)
-            {
-                qDebug() << QString::fromStdString(std::string(ex.what()));
-            }
-            catch (...)
-            {
-                qDebug() << "unknow exception";
-            }
-            self->w.disconnect();
-            return decltype(ptr_run(T()))();
-        });
-        return t;
-    }
+    //template <typename F>
+    //auto thenAsync(F ptr_run) -> std::enable_if_t<!std::is_same_v<decltype(ptr_run(T())), void>, std::shared_ptr<prismQtTask<decltype(ptr_run(T()))>>>
+    //{
+    //    auto self = this->shared_from_this();
+    //    std::shared_ptr<prismQtTask<decltype(ptr_run(T()))>> t = std::make_shared<prismQtTask<decltype(ptr_run(T()))>>();
+    //    w.setFuture(futureResult);
+    //    t->futureResult = QtConcurrent::run([self, ptr_run] {
+    //        while (!self->futureResult.isFinished())
+    //            QCoreApplication::processEvents();
+    //        try
+    //        {
+    //            return ptr_run(self->w.result());
+    //        }
+    //        catch (std::exception& ex)
+    //        {
+    //            qDebug() << QString::fromStdString(std::string(ex.what()));
+    //        }
+    //        catch (...)
+    //        {
+    //            qDebug() << "unknow exception";
+    //        }
+    //        self->w.disconnect();
+    //        return decltype(ptr_run(T()))();
+    //    });
+    //    return t;
+    //}
 };
 class prismQtTaskManager : public QObject
 {
