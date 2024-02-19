cf7a2926911066b4e9a3bdcf9369a1f51390f499
huangbinbin <huangbinbin@deepvision-tech.com>
2024-02-19 13:11:56 +0800

    feat - add prism enum proxy

---
+++ include/prism/qt/core/hpp/prismEnumProxy.hpp
@@ -0,0 +1,142 @@
+#ifndef PRISMENUMPROXY_HPP
+#define PRISMENUMPROXY_HPP
+
+#include "../prismQt_core_global.h"
+#include <QObject>
+#include <QString>
+#include <prism/prism.hpp>
+#include <QQmlEngine>
+namespace prism::qt::core{
+
+class PRISMQT_CORE_EXPORT prismEnumProxyBase : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QStringList options READ options WRITE setOptions NOTIFY optionsChanged)
+    Q_PROPERTY(QString valueStr READ valueStr WRITE setValueStr NOTIFY valueStrChanged)
+    Q_PROPERTY(int value READ value WRITE setValue NOTIFY valueChanged)
+
+public:
+    explicit prismEnumProxyBase(QObject* parent = nullptr):QObject(parent)
+    {
+        QQmlEngine::setObjectOwnership(this, QQmlEngine::ObjectOwnership::CppOwnership);
+    }
+
+    const QStringList &options() const
+    {
+        return m_options;
+    }
+    void setOptions(const QStringList &newOptions)
+    {
+        if (m_options == newOptions)
+            return;
+        m_options = newOptions;
+        emit optionsChanged();
+    }
+
+    const QString &valueStr() const
+    {
+        return m_valueStr;
+    }
+    virtual void setValueStr(const QString &newValueStr)
+    {
+        if (m_valueStr == newValueStr)
+            return;
+        m_valueStr = newValueStr;
+        emit valueStrChanged();
+    }
+
+    int value() const
+    {
+        return m_value;
+    }
+    virtual void setValue(int newValue)
+    {
+        if (m_value == newValue)
+            return;
+        m_value = newValue;
+        emit valueChanged();
+    }
+
+    virtual int qstr2int(QString svalue){Q_UNUSED(svalue) return 0;}
+    virtual QString int2qstr(int ivalue){ Q_UNUSED(ivalue) return QString();}
+
+
+    QStringList m_options {};
+    QString m_valueStr = "";
+    int m_value = 0;
+
+signals:
+    void optionsChanged();
+
+    void valueStrChanged();
+
+    void valueChanged();
+
+private:
+};
+
+
+
+template<class T>
+class PRISMQT_CORE_EXPORT prismEnumProxy : public prismEnumProxyBase
+{
+public:
+    T getEnumValue()
+    {
+        return static_cast<T>(value());
+    }
+    virtual int qstr2int(QString svalue)
+    {
+        return static_cast<int>(prism::enums::enum_info<T>::fromstring(svalue.toStdString().c_str()));
+    }
+    virtual QString int2qstr(int ivalue)
+    {
+        return QString::fromStdString(std::string(prism::enums::enum_info<T>::tostring(static_cast<T>(ivalue))));
+    }
+    prismEnumProxy(QObject* parent = nullptr):prismEnumProxyBase(parent)
+    {
+        QQmlEngine::setObjectOwnership(this, QQmlEngine::ObjectOwnership::CppOwnership);
+        static QStringList qops ;
+        if(!qops.size())
+        {
+            std::vector<std::string>& ops = prism::enums::enum_info<T>::getStrVector();
+            for(const std::string& item : ops)
+            {
+                qops.append(QString::fromStdString(item));
+            }
+        }
+        setOptions(qops);
+        setValueStr(qops.at(m_value));
+    }
+    void setValue(int newValue) override
+    {
+        if (m_value == newValue)
+            return;
+        m_value = newValue;
+        emit valueChanged();
+        if(newValue>=0 && newValue < options().size())
+        {
+            QString newstr = options().at(newValue);
+            setValueStr(newstr);
+        }
+    }
+
+    void setValueStr(const QString &newValueStr) override
+    {
+        if (m_valueStr == newValueStr)
+            return;
+        m_valueStr = newValueStr;
+        emit valueStrChanged();
+        int idx = options().indexOf(newValueStr);
+        if(idx >=0 && idx < options().size())
+        {
+            setValue(idx);
+        }
+    }
+};
+
+
+
+} //namespace prism::qt::core
+
+#endif // PRISMENUMPROXY_HPP
--- include/prism/qt/core/hpp/prismQt.hpp
+++ include/prism/qt/core/hpp/prismQt.hpp
@@ -1,6 +1,7 @@
 #ifndef PRISM_QT_CORE_HPP_PRISMQT_HPP
 #define PRISM_QT_CORE_HPP_PRISMQT_HPP
 
+#include "prismEnumProxy.hpp"
 #include "prismModelListProxy.hpp"
 #include "prismTreeModelProxy.hpp"
 #include <prism/prism.hpp>
@@ -9,25 +10,38 @@
 #include <QVariant>
 
 Q_DECLARE_METATYPE(std::string)
+Q_DECLARE_METATYPE(std::string*)
+Q_DECLARE_METATYPE(std::shared_ptr<std::string>)
 Q_DECLARE_METATYPE(const char*)
 Q_DECLARE_METATYPE(QList<std::string>)
 Q_DECLARE_METATYPE(QVector<std::string>)
 Q_DECLARE_METATYPE(std::list<std::string>)
 Q_DECLARE_METATYPE(std::vector<std::string>)
+Q_DECLARE_METATYPE(bool*)
 Q_DECLARE_METATYPE(std::shared_ptr<bool>)
 Q_DECLARE_METATYPE(std::stdoptional<bool>)
+Q_DECLARE_METATYPE(float*)
+Q_DECLARE_METATYPE(std::shared_ptr<float>)
 Q_DECLARE_METATYPE(std::stdoptional<float>)
+Q_DECLARE_METATYPE(double*)
+Q_DECLARE_METATYPE(std::shared_ptr<double>)
 Q_DECLARE_METATYPE(std::stdoptional<double>)
+Q_DECLARE_METATYPE(long*)
+Q_DECLARE_METATYPE(std::shared_ptr<long>)
 Q_DECLARE_METATYPE(std::stdoptional<long>)
 Q_DECLARE_METATYPE(std::stdoptional<ulong>)
 Q_DECLARE_METATYPE(std::stdoptional<qlonglong>)
 Q_DECLARE_METATYPE(std::stdoptional<qulonglong>)
 Q_DECLARE_METATYPE(std::stdoptional<int8_t>)
 Q_DECLARE_METATYPE(std::stdoptional<int16_t>)
+Q_DECLARE_METATYPE(int*)
+Q_DECLARE_METATYPE(std::shared_ptr<int>)
 Q_DECLARE_METATYPE(std::stdoptional<int32_t>)
 Q_DECLARE_METATYPE(std::stdoptional<uint>)
 Q_DECLARE_METATYPE(std::stdoptional<uint8_t>)
 Q_DECLARE_METATYPE(std::stdoptional<uint16_t>)
+Q_DECLARE_METATYPE(prism::qt::core::prismEnumProxyBase*)
+Q_DECLARE_METATYPE(std::shared_ptr<prism::qt::core::prismEnumProxyBase>)
 
 #define PRISMQT_TYPE(Class)                     \
 Q_DECLARE_METATYPE(Class)                   \
@@ -38,6 +52,8 @@ Q_DECLARE_METATYPE(std::list<Class>)        \
 Q_DECLARE_METATYPE(QList<Class>)            \
 Q_DECLARE_METATYPE(std::vector<Class>)      \
 Q_DECLARE_METATYPE(QVector<Class>)\
+Q_DECLARE_METATYPE(prism::qt::core::prismEnumProxy<Class>*)                 \
+Q_DECLARE_METATYPE(std::shared_ptr<prism::qt::core::prismEnumProxy<Class>>)                 \
 Q_DECLARE_METATYPE(prism::qt::core::prismModelProxy<Class>*)                 \
 Q_DECLARE_METATYPE(std::shared_ptr<prism::qt::core::prismModelProxy<Class>>) \
 Q_DECLARE_METATYPE(prism::qt::core::prismModelListProxy<Class>*)             \
